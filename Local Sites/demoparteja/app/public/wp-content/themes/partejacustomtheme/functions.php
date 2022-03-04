82098efd8665df707563f060eb402b2aae09bd7b
Jerry Frederic <jfpl.dev@gmail.com>
2022-03-04 08:21:22 +0100

    set dynamic style version

--- Local Sites/demoparteja/app/public/wp-content/themes/partejacustomtheme/functions.php
+++ Local Sites/demoparteja/app/public/wp-content/themes/partejacustomtheme/functions.php
@@ -2,8 +2,9 @@
 
 function parteja_register_styles()
 {
-
-    wp_enqueue_style('parteja-style', get_template_directory_uri() . "/theme-template/css/style.css", array('parteja-bootstrap'), '1.0', 'all');
+    //dynamic version set
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('parteja-style', get_template_directory_uri() . "/theme-template/css/style.css", array('parteja-bootstrap'), $version, 'all');
     wp_enqueue_style('parteja-bootstrap', get_template_directory_uri() . "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('parteja-fontawesome', get_template_directory_uri() . "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
